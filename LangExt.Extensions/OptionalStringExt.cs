479509e3df227f931a9553d99f2b98d75bd1176d
Stanley Gehman <Stanley@HersheysMM.com>
2020-09-02 15:43:24 -0500

    Add OptionalStringExt extension methods from Joe Wittmer.

---
+++ LangExt.Extensions/OptionalStringExt.cs
@@ -0,0 +1,31 @@
+﻿using LanguageExt;
+using static LanguageExt.Prelude;
+
+namespace LangExt.Extensions
+{
+    public static class OptionalStringExt
+    {
+        public static Option<string> EmptyAsNone1(this string source)
+            => source.Apply(Optional)
+                .Match(
+                    x => string.IsNullOrEmpty(x)
+                        ? Option<string>.None
+                        : Some(x),
+                    () => Option<string>.None
+                );
+
+        public static Option<string> EmptyAsNone2(this string source) =>
+            source
+                .Apply(Optional)
+                .Filter(x => !string.IsNullOrEmpty(x));
+
+        public static Option<string> EmptyAsNone(this string source) =>
+            source
+                .Apply(Optional)
+                .EmptyAsNone();
+
+        public static Option<string> EmptyAsNone(this Option<string> optStr) =>
+            optStr
+                .Filter(x => x != string.Empty);
+    }
+}

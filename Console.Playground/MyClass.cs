59d27a6e92fc0ee6e8bdbb5f4b08f2c6b3bb4416
Stanley Gehman <Stanley@HersheysMM.com>
2022-05-19 17:17:20 -0400

    Build prototye for failing MapAsync function.

---
+++ Console.Playground/MyClass.cs
@@ -0,0 +1,8 @@
+﻿namespace Console.Playground
+{
+    class MyClass
+    {
+        public string Name { get; private set; }
+        public static MyClass New(string n) => new() { Name = n };
+    }
+}
--- Console.Playground/Program.cs
+++ Console.Playground/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using LangExt.Extensions;
 using LanguageExt;
 using static LanguageExt.Prelude;
@@ -11,9 +12,85 @@ namespace Console.Playground
         static void Main(string[] args)
         {
             //TestEmptyAsNone();
+            TestMapAsync().Wait();
 
             WriteLine("Press enter to quit...");
             ReadLine();
+        }
+
+        private static async Task TestMapAsync()
+        {
+            var a = await Some(3).MapAsync(async _ => {
+                await Task.Delay(500);
+                WriteLine("Some Done <Unit>!");
+                return unit;
+            });
+            
+            var a_n = await Option<Unit>.None.MapAsync(async _ => {
+                await Task.Delay(500);
+                WriteLine("None Done <Unit>!");
+                return unit;
+            });
+
+            var b = await Some(3).MapAsync<int, Option<Unit>>(async _ => {
+                await Task.Delay(500);
+                WriteLine("Some Done Option<Unit>!");
+                return unit;
+            });
+            
+            var b_n = await Option<int>.None.MapAsync<int, Option<Unit>>(async _ => {
+                await Task.Delay(500);
+                WriteLine("None Done Option<Unit>!");
+                return unit;
+            });
+
+            var c = await Some(3).MapAsync(async val => {
+                await Task.Delay(500);
+                WriteLine("Some Done <bool>!");
+                return val < 5;
+            });
+            
+            var c_n = await Option<int>.None.MapAsync(async val => {
+                await Task.Delay(500);
+                WriteLine("None Done <bool>!");
+                return val < 5;
+            });
+
+            var d = await Some(3).MapAsync<int, Option<bool>>(async val => {
+                await Task.Delay(500);
+                WriteLine("Some Done Option<bool>!");
+                return val < 5;
+            });
+
+            var d_n = await Option<int>.None.MapAsync<int, Option<bool>>(async val => {
+                await Task.Delay(500);
+                WriteLine("None Done Option<bool>!");
+                return val < 5;
+            });
+
+            var e = await Some(MyClass.New("")).MapAsync(async _ => {
+                await Task.Delay(500);
+                WriteLine("Some Done <MyClass>!");
+                return MyClass.New("Stanley");
+            });
+
+            var e_n = await Option<MyClass>.None.MapAsync(async _ => {
+                await Task.Delay(500);
+                WriteLine("None Done <MyClass>!");
+                return MyClass.New("Stanley");
+            });
+
+            var f = await Some(MyClass.New("")).MapAsync<MyClass, Option<MyClass>>(async _ => {
+                await Task.Delay(500);
+                WriteLine("Some Done Option<MyClass>!");
+                return MyClass.New("Stanley");
+            });
+            
+            var f_n = await Option<MyClass>.None.MapAsync<MyClass, Option<MyClass>>(async _ => {
+                await Task.Delay(500);
+                WriteLine("None Done Option<MyClass>!");
+                return MyClass.New("Stanley");
+            });
         }
 
         private static void TestEmptyAsNone()
